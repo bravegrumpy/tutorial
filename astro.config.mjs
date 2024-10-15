6644e505978d2842466ac647acb79efbb659b7a0
Robin Zollner <code@bravegrumpy.com>
2024-10-14 21:48:35 -0400

    adding random Greeting component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrobravegrumpy.netlify.app"
+  site: "https://astrobravegrumpy.netlify.app",
+  integrations: [preact()]
 });
